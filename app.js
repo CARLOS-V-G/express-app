be80f9639ac3cf1cad9e3e788f5598ba0d7b00df
CARLOS-V-G <cv5944341@gmail.com>
2022-11-24 11:05:01 -0300

    mi primer expres

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const app = express();
 
 const port = 8070;
 
+const path = require('path')
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views','home.html')))
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views','login.html')))
+app.get('/registro', (req,res) => res.sendfile(path.join(__dirname, 'views','registro.html')))
+app.get('/redes', (req,res) => res.sendfile(path.join(__dirname, 'views','redes.html')))
+app.get('/perfil', (req,res) => res.sendfile(path.join(__dirname, 'views','perfil.html')))
+app.get('*', (req,res) => res.sendfile(path.join(__dirname, 'views','404.html')))
+
 
 
 app.listen(port, () => console.log(`servidor levantada en el port ${port} `))
